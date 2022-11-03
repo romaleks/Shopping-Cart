aeff908ebcb9f701a1735147e77d3292ac2b555b
IDDna <romanov1371@gmail.com>
2022-11-03 17:49:19 +0300

    Set up basic structure of the site

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+import Shop from './components/Shop'
 
 const App = () => {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
 
 export default App
