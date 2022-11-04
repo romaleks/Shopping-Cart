58653dd5f51457032ef7110256d23320bbc77785
IDDna <romanov1371@gmail.com>
2022-11-04 21:15:33 +0300

    Create and style home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
 const Home = () => {
-  // return <main>Home</main>
+  return (
+    <main className='home'>
+      <div className='content'>
+        <h2 className='title'>
+          The most <span>beautifull</span> and <span>high-quality</span> sneackers <br />
+          in the whole wolrd
+        </h2>
+        <h3 className='subtitle'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus tempora
+          expedita consectetur tenetur veritatis eligendi. Veritatis laboriosam quos cum
+          optio?
+        </h3>
+        <button className='btn'>Explore Now</button>
+      </div>
+      <div className='preview'></div>
+    </main>
+  )
 }
 
 export default Home
