a26ac0d2f18db5fa5ea4b147daa51e704eab80e1
IDDna <romanov1371@gmail.com>
2022-11-05 21:01:40 +0300

    Add more sneakers and 'see more' button

--- src/components/utils/CardsGrid.js
+++ src/components/utils/CardsGrid.js
@@ -6,6 +6,10 @@ import sneak3 from '../../assets/sneakers/sneak3.png'
 import sneak4 from '../../assets/sneakers/sneak4.png'
 import sneak5 from '../../assets/sneakers/sneak5.png'
 import sneak6 from '../../assets/sneakers/sneak6.png'
+import sneak7 from '../../assets/sneakers/sneak7.png'
+import sneak8 from '../../assets/sneakers/sneak8.png'
+import sneak9 from '../../assets/sneakers/sneak9.png'
+import { ReactComponent as Arrow } from '../../assets/arrow.svg'
 
 const CardsGrid = () => {
   return (
@@ -13,9 +17,15 @@ const CardsGrid = () => {
       <Card title='Nike Dunk Low Sesame' price={300} image={sneak1} />
       <Card title='Nike Dunk High Lakers' price={180} image={sneak5} />
       <Card title='Nike Dunk Low Patent Halloween' price={140} image={sneak3} />
-      <Card title='Nike Air Max 90 Flyleather Ruohan Wang' price={235} image={sneak4} />
+      <Card title='Nike Air Max 90 Flyleather Ruohan' price={235} image={sneak4} />
+      <Card title='Air Jordan 1 Low SE Red Quilt' price={280} image={sneak8} />
       <Card title='Nike Dunk Low Arizona State' price={180} image={sneak2} />
-      <Card title='Nike Dunk High White Green' price={165} image={sneak6} />
+      {/* <Card title='Nike Dunk High White Green' price={165} image={sneak6} />
+      <Card title='Air Jordan 1 Low SE Paint Drip' price={405} image={sneak7} />
+      <Card title='Air Jordan 1 High OG Heritage' price={200} image={sneak9} /> */}
+      <div className='more'>
+        See More <Arrow />
+      </div>
     </div>
   )
 }
