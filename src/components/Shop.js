5a22ca85cda0c94b13c39ce81fd8f2058f4cdd70
IDDna <romanov1371@gmail.com>
2022-11-05 20:39:39 +0300

    Style shop page

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,8 +4,8 @@ import CardsGrid from './utils/CardsGrid'
 const Shop = () => {
   return (
     <main>
-      <div className='Shop'>
-        <h2>Our best collection</h2>
+      <div className='shop'>
+        <h2 className='title'>Our best collection</h2>
         <CardsGrid />
       </div>
     </main>
