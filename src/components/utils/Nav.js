7c7a31f1b79b140c28a28e8530857cc5711f25bb
IDDna <romanov1371@gmail.com>
2022-11-06 17:08:04 +0300

    Implement activating navigation links

--- src/components/utils/Nav.js
+++ src/components/utils/Nav.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
+  const setActive = e => {
+    e.target.classList.add('active')
+  }
+
   return (
-    <nav>
-      <ul className='container nav gap'>
-        <Link to='/'>
-          <li className='nav-item active'>Home</li>
-        </Link>
-        <Link to='/shop'>
-          <li className='nav-item'>Shop</li>
-        </Link>
-        <li className='nav-item'>Contacts</li>
-      </ul>
+    <nav className='container nav gap'>
+      <NavLink
+        to='/'
+        className={({ isActive }) => (isActive ? 'nav-item active' : 'nav-item')}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to='/shop'
+        className={({ isActive }) => (isActive ? 'nav-item active' : 'nav-item')}
+      >
+        Shop
+      </NavLink>
+      <a href='#' className='nav-item'>
+        Contacts
+      </a>
     </nav>
   )
 }
