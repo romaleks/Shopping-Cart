f3f65f79f6ad272aeac687c3112a88849d6bee70
IDDna <romanov1371@gmail.com>
2022-11-11 20:32:12 +0300

    Add dark background on cart opens

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,33 +9,36 @@ const Cart = ({ status, closeCart, items }) => {
   }
 
   return (
-    <div className={`cart ${status ? 'active' : ''}`}>
-      <div className='close-btn' onClick={closeCart}></div>
-      <div className='title'>Cart</div>
-      <div className='items'>
-        {items.map((item, index) => {
-          const { title, price, image, amount } = item
+    <>
+      <div className={`overlay ${status ? 'active' : ''}`} onClick={closeCart}></div>
+      <div className={`cart ${status ? 'active' : ''}`}>
+        <div className='close-btn' onClick={closeCart}></div>
+        <div className='title'>Cart</div>
+        <div className='items'>
+          {items.map((item, index) => {
+            const { title, price, image, amount } = item
 
-          return (
-            <div key={index} className='item'>
-              <div className='image'>
-                <img src={image} alt='' />
-                <div className='amount'>{amount}</div>
+            return (
+              <div key={index} className='item'>
+                <div className='image'>
+                  <img src={image} alt='' />
+                  <div className='amount'>{amount}</div>
+                </div>
+                <div className='info'>
+                  <div className='item-title'>{title}</div>
+                  <div className='price'>${price}</div>
+                </div>
               </div>
-              <div className='info'>
-                <div className='item-title'>{title}</div>
-                <div className='price'>${price}</div>
-              </div>
-            </div>
-          )
-        })}
+            )
+          })}
+        </div>
+        <div className='total'>
+          <div className='total-title'>Total:</div>
+          <div className='total-price'>${computeTotal()}</div>
+        </div>
+        <div className='checkout-btn btn'>checkout</div>
       </div>
-      <div className='total'>
-        <div className='total-title'>Total:</div>
-        <div className='total-price'>${computeTotal()}</div>
-      </div>
-      <div className='checkout-btn btn'>checkout</div>
-    </div>
+    </>
   )
 }
 
